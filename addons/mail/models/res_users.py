54abb596225d584f16d67f5c5cc495f151dc2e90
Sara <saratahirisasa1994@gmail.com>
2022-01-01 12:02:16 -0800

    ajustement_3

--- addons/mail/models/res_users.py
+++ addons/mail/models/res_users.py
@@ -104,7 +104,46 @@ GROUP BY channel_moderator.res_users_id""", [tuple(self.ids)])
         elif sel_groups:
             self.env['mail.channel'].search([('group_ids', 'in', sel_groups)])._subscribe_users()
         return write_res
-
+    
+    @api.model
+    def systray_get_leads(self):
+        model_name = "crm.lead"
+        query = """SELECT m.id, count(*), act.res_model as model,
+                    CASE
+                        WHEN %(today)s::date - act.date_deadline::date = 0 Then 'today'
+                        WHEN %(today)s::date - act.date_deadline::date > 0 Then 'overdue'
+                        WHEN %(today)s::date - act.date_deadline::date < 0 Then 'planned'
+                    END AS states
+                FROM mail_activity AS act
+                JOIN ir_model AS m ON act.res_model_id = m.id
+                WHERE user_id = %(user_id)s AND m.model = %(model_name)s
+                GROUP BY m.id, states, act.res_model;
+                """
+        self.env.cr.execute(query, {
+            'today': fields.Date.context_today(self),
+            'user_id': self.env.uid,
+            'model_name': model_name
+        })
+        icon_ = modules.module.get_module_icon(self.env['crm.lead']._original_module)
+        activity_data = self.env.cr.dictfetchall()
+        if( not activity_data):
+            return []
+        
+        user_activities = {
+                'name': 'CRM qualification',
+                'model': model_name,
+                'type': 'activity',
+                'icon': icon_,
+                'total_count': len(activity_data),
+                'today_count': 0,
+                'overdue_count': 0,
+                'planned_count': 0
+        }
+        
+        for activity in activity_data:
+            user_activities['%s_count' % activity['states']] += activity['count']
+        return [user_activities,]
+    
     @api.model
     def systray_get_activities(self):
         query = """SELECT m.id, count(*), act.res_model as model,
